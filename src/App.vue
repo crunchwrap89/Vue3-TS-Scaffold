dbca3bcf3ee6eb625848b1cdc58153fcfa423501
Marcus N <marcus.nilsson@wintergarde.se>
2022-10-13 21:18:55 +0200

    removed unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-    <router-view />
     <div style="display:flex; justify-content: center; align-items: center; height: 100%; color: var(--white)">
         <h1> &#x2605; Success &#x2605;</h1>
     </div>
